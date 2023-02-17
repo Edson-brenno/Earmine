ced256a2833dfae09ce1e1409fb680c783a6bc72
brenno <brennorebeiro@gmail.com>
2023-02-17 09:54:03 -0300

    finaliza jogo pablo

---
+++ script.js
@@ -0,0 +1,36 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+
+function jump() {
+    if (dino.classList != "jump") {
+        dino.classList.add("jump");
+
+        setTimeout(function () {
+            dino.classList.remove("jump");
+        }, 300);
+    }
+}
+
+let isAlive = setInterval(() => {
+    let randomTime = Math.random() * 6000; //criar cactus de maneira aleatória
+
+    // dino posição
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+
+    // cactus posição
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+
+    // analisar se ambos se tocam
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140){
+        alert("Vixe, game over :(");
+        document.body.innerHTML = '<h1 class="game-over""> Atualize a página e jogue novamente </h1>'; // criando título ao final do jogo
+
+        window.location.href = 'index.html'
+    }
+
+    setTimeout(isAlive, randomTime);
+}, 20);
+
+document.addEventListener("keydown", function (event) {
+    jump();
+});
